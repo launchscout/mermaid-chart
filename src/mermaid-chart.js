388016b3d7f053a3d6cbb0dc6cf524774b196789
Chris Nelson <superchrisnelson@gmail.com>
2024-08-18 20:24:55 -0400

    initial commit

---
+++ src/mermaid-chart.js
@@ -0,0 +1,29 @@
+import { LitElement, html } from "lit";
+import mermaid from "mermaid";
+
+class MermaidChart extends LitElement {
+  static properties = {
+    source: {},
+    config: {
+      type: Object
+    }
+  }
+
+  render() {
+    return html`<div id="chart"></div>`;
+  }
+
+  get chartElement() {
+    return this.shadowRoot.querySelector('#chart');
+  }
+  async updated() {
+    mermaid.initialize(this.config);
+    if (this.source) {
+      const {svg, bindEvents} = await mermaid.render('mermaidChart', this.source);
+      this.chartElement.innerHTML = svg;
+    }
+  }
+
+}
+customElements.define('mermaid-chart', MermaidChart);
+export { MermaidChart };
